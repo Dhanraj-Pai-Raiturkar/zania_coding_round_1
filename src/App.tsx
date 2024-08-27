ef40918a56586e351133dc96f9882fd18cbda50f
Dhanraj Pai Raiturkar <dhanraj.raiturkar@gmail.com>
2024-08-27 13:32:07 +0530

    card component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
 import "./App.css";
 import Data from "./static/data.json";
+import Card from "./components/card/Card";
+import DataInterface from "./interface/data";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        {Data.map((item) => {
-          return <div className="col">{item.title}</div>;
+        {Data.map((item: DataInterface, index: number) => {
+          return (
+            <div className="col">
+              <Card
+                key={`card${index}`}
+                title={item.title}
+                type={item.type}
+                position={item.position}
+                image={item.image}
+              />
+            </div>
+          );
         })}
       </div>
     </div>
